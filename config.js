623532ce1b7ee366a757f86a7f1c9f4da60e6882
RealEnzoTDZ <45722427+RealEnzoTDZ@users.noreply.github.com>
2021-02-17 12:29:42 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  "token": "Nzc1NzAxMjcxODc3MTI0MTM2.X6qKGw.X16_EMliU7-Id-tdXCXBdSqSP-8", // https://discordapp.com/developers/applications/ID/bot
-  "mongodbUrl": "mongodb+srv://glitch:adminuserforglitch.comsonyxcanwork*243@deathroad-lol15.mongodb.net/discordServers", // Mongodb connection url.
+  "token": "HIDDEN", // https://discordapp.com/developers/applications/ID/bot
+  "mongodbUrl": "mongodb+srv://HIDDEN", // Mongodb connection url.
   "id": "775701271877124136", // https://discordapp.com/developers/applications/ID/information
-  "clientSecret": "EgTewTcpLyxwH6RselsaJ-CyzFUIkoR1", // https://discordapp.com/developers/applications/ID/information
+  "clientSecret": "HIDDEN", // https://discordapp.com/developers/applications/ID/information
   "domain": "http://nyxclient.com",
   "port": 80,
   "hostPort": 3000
